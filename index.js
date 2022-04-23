c8f4421cd8edbdc939e51d7e4c19224e4911339a
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-23 12:43:16 +0600

    connected to mongodb, imported data to mongodb

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 
 
@@ -10,10 +12,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Running Genius Car Server');
 });
 
-app.listen(port, ()=>{
-    console.log('Listening to port',port);
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntqok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('service');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
 })
