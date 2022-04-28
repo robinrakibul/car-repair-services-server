62e7832cbe3a670a4b21f29621de5766471aa7a7
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-28 16:23:04 +0600

    done added orders

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ async function run() {
 
         // Order API
         app.get('/orders',async(req,res)=>{
-            const query = {};
+            const email = req.query.email;
+            const query = {email: email};
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
